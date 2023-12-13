920e57e9fcf0e1286ebaab24cc8b37e4dc604b22
ecostabal <ecostabal@urbex.cl>
2023-12-13 12:22:07 -0300

    workingnow

--- function.js
+++ function.js
@@ -1,174 +1,211 @@
 const axios = require('axios');
 
-// Apikey (Reemplaza 'TU_API_KEY' con tu clave de API de Monday.com)
+// Apikey (Replace 'YOUR_API_KEY' with your Monday.com API key)
 const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE';
 
-// Función para obtener los datos de un ítem en Monday.com
+// Function to get the data of an item from Monday.com
 async function getMondayItemData(itemId) {
     try {
         const query = `
-          query {
-            items(ids: [${itemId}]) {
-              column_values {
-                id
-                text
-              }
-              subitems {
-                column_values {
-                  id
-                  text
+            query {
+                items(ids: [${itemId}]) {
+                    column_values {
+                        id
+                        text
+                        value
+                    }
+                    subitems {
+                        column_values {
+                            id
+                            text
+                            value
+                        }
+                    }
                 }
-              }
             }
-          }
         `;
 
         const response = await axios.post('https://api.monday.com/v2', { query }, {
-          headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json'
-          }
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
         });
 
         if (response.data && response.data.data && response.data.data.items && response.data.data.items.length > 0) {
             const itemData = response.data.data.items[0];
-            console.log('Datos del item y subitems obtenidos con éxito:', itemData);
+            console.log('Data of item and its subitems retrieved successfully:', itemData);
 
-            // Aquí tienes acceso a las columnas del ítem y sus subelementos
             const columnsData = itemData.column_values;
             const subitemsData = itemData.subitems;
 
-            // Ahora puedes imprimir y retornar ambos, columnas y subelementos
-            console.log('Columnas del Item:', columnsData);
-            console.log('Subelementos:', subitemsData);
+            console.log('Columns of the Item:', JSON.stringify(columnsData, null, 2));
+            console.log('Subitems:', JSON.stringify(subitemsData, null, 2));
 
             return { columnsData, subitemsData };
         } else {
-            return {}; // Devuelve un objeto vacío si no hay datos
+            console.error('No items found for the given ID:', itemId);
+            return {};
         }
     } catch (error) {
-        console.error('Error al obtener datos del item:', error);
+        console.error('Error occurred while retrieving the data of the item:', error);
         throw error;
     }
 }
 
-// Función para procesar subelementos y crear nuevos ítems
+// Function to process subitems and create new items
 async function processSubElementsAndCreateItems(boardId, itemId) {
     try {
-        console.log('Iniciando procesamiento de subelementos y creación de nuevos ítems para el item:', itemId);
+        console.log('Starting processing of subitems and creation of new items for the item:', itemId);
 
         const itemData = await getMondayItemData(itemId);
         if (!itemData || Object.keys(itemData).length === 0 || !itemData.columnsData) {
-            console.error('No se encontraron datos de columnas para el item con ID:', itemId);
+            console.error('No columns data found for the item with ID:', itemId);
             return;
         }
 
         const { columnsData, subitemsData } = itemData;
-
-        // Definir formattedLocation antes de su uso
-        let formattedLocation = "";
-
-        const locationColumn = columnsData.find(cv => cv.id === "location");
+        let formattedLocation = '';
+        const locationColumn = columnsData.find(cv => cv.id === 'ubicaci_n');
         if (locationColumn && locationColumn.text) {
-            try {
-                const locationData = JSON.parse(locationColumn.text);
-                if (locationData && 'lat' in locationData && 'lng' in locationData) {
-                    // Formatear la ubicación como una cadena de texto simple
-                    formattedLocation = `${locationData.lat} ${locationData.lng}`;
-                    if (locationData.address) {
-                        formattedLocation += ` ${locationData.address}`;
-                    }
+            const locationData = JSON.parse(locationColumn.value);
+            if (locationData && 'lat' in locationData && 'lng' in locationData) {
+                formattedLocation = `${locationData.lat} ${locationData.lng}`;
+                console.log('Processing location data:', locationColumn);
+                if (locationData.address) {
+                    formattedLocation += ` ${locationData.address}`;
                 }
-            } catch (error) {
-                console.error('Error al analizar los datos de ubicación:', error);
             }
         }
 
-        if (!subitemsData || subitemsData.length === 0) {
-            console.log('No hay subelementos que procesar para este item.');
+        if (!Array.isArray(subitemsData) || subitemsData.length === 0) {
+            console.log('No subitems to process for this item.');
             return;
         }
 
         for (const subitem of subitemsData) {
             const subitemColumns = subitem.column_values;
-            const tipoFirmanteColumn = subitemColumns.find(column => column.id === 'firmante');
+            console.log('Subitem column values:', JSON.stringify(subitemColumns, null, 2));
+            const signerTypeColumn = subitemColumns.find(column => column.id === 'reflejo_189');
 
-            if (tipoFirmanteColumn && (tipoFirmanteColumn.text === 'Arrendador' || tipoFirmanteColumn.text === 'Arrendatario')) {
-                // Datos subitem
-                const nombre = subitemColumns.find(column => column.id === 'nombre')?.text || '';
-                const apellido = subitemColumns.find(column => column.id === 'apellido')?.text || '';
-                const rut = subitemColumns.find(column => column.id === 'rut')?.text || '';
-                const telefono = subitemColumns.find(column => column.id === 'telefono')?.text || '';
-                const correo = subitemColumns.find(column => column.id === 'correo')?.text || '';
-                const nombreCompleto = `${nombre} ${apellido}`;
+            if (signerTypeColumn && (signerTypeColumn.text === 'Arrendador' || signerTypeColumn.text === 'Arrendatario')) {
+                const name = subitemColumns.find(column => column.id === 'reflejo0')?.text || '';
+                const lastName = subitemColumns.find(column => column.id === 'reflejo')?.text || '';
+                const id = subitemColumns.find(column => column.id === 'reflejo_1')?.text || '';
+                const phone = subitemColumns.find(column => column.id === 'reflejo_2')?.text || '';
+                const email = subitemColumns.find(column => column.id === 'reflejo_3')?.text || '';
+                const fullName = `${name} ${lastName}`;
 
-                // Datos item
-                const tipoContrato = columnsData.estado_1?.text || '';
-                const tipoPropiedad = columnsData.estado_17?.text || '';
-                const numeroUnidad = columnsData.texto?.text || '';
-                const estacionamientos = columnsData.texto4?.text || '';
-                const bodegas = columnsData.texto2?.text || '';
-                const valorArriendo = columnsData.n_meros?.text || '';
-                const gastoNotarial = columnsData.n_meros5?.text || '';
+                const contractType = columnsData.find(cv => cv.id === 'estado_1')?.text || '';
+                const propertyType = columnsData.find(cv => cv.id === 'estado_17')?.text || '';
+                const unitNumber = columnsData.find(cv => cv.id === 'texto')?.text || '';
+                const parkingSpaces = columnsData.find(cv => cv.id === 'texto4')?.text || '';
+                const storageUnits = columnsData.find(cv => cv.id === 'texto2')?.text || '';
+                const rentalValue = columnsData.find(cv => cv.id === 'n_meros')?.text || '';
+                const notaryFee = columnsData.find(cv => cv.id === 'n_meros5')?.text || '';
 
-                // Crear el nombre del nuevo item
-                const nuevoItemNombre = `OT - ${nombreCompleto}`;
-
-                // Crear nuevo item con datos específicos
+                const newItemName = `OT - ${fullName}`;
                 const newItemData = {
-                    estado8: tipoContrato,
-                    estado: tipoPropiedad,
+                    estado8: contractType,
+                    estado: propertyType,
                     ubicaci_n: formattedLocation,
-                    texto9: numeroUnidad,
-                    texto5: estacionamientos,
-                    texto95: bodegas,
-                    n_meros: valorArriendo,
-                    n_meros9: gastoNotarial,
-                    texto6: nombreCompleto,
-                    texto: nombre,
-                    texto4: apellido,
-                    texto3: rut,
-                    tel_fono: telefono,
-                    correo_electr_nico: correo,
-                    // Agrega aquí más campos necesarios
+                    texto9: unitNumber,
+                    texto5: parkingSpaces,
+                    texto95: storageUnits,
+                    n_meros: rentalValue,
+                    n_meros9: notaryFee,
+                    texto6: fullName,
+                    texto: name,
+                    texto4: lastName,
+                    texto3: id,
+                    tel_fono: phone,
+                    correo_electr_nico: JSON.stringify({ email: email, text: email }),
                 };
 
-                // Crear el nuevo item en el otro tablero con el nombre personalizado
-                const newItemId = await createNewItemInOtherBoard(boardId, nuevoItemNombre, newItemData, formattedLocation);
-                console.log('Subelemento procesado con éxito:', tipoFirmanteColumn.text, ' - Nuevo ítem creado con ID:', newItemId);
+                const newItemId = await createNewItemInOtherBoard(boardId, newItemName, newItemData, formattedLocation);
+                console.log('Processed subitem:', signerTypeColumn.text, '- New item created with ID:', newItemId);
             }
         }
     } catch (error) {
-        console.error('Error al procesar subelementos y crear nuevos ítems:', error);
+        console.error('Error occurred while processing subitems and creating new items:', error);
+        throw error;
+    }
+}
+
+// Function to create a new item in a different board
+async function createNewItemInOtherBoard(boardId, name, data, formattedLocation) {
+    try {
+        // Convertir los datos de las columnas a un string JSON para la mutación
+        const columnValues = JSON.stringify(data);
+
+        // Crear la mutación GraphQL
+        const mutation = `
+            mutation createItem($boardId: Int!, $itemName: String!, $columnValues: JSON!) {
+                create_item (board_id: $boardId, item_name: $itemName, column_values: $columnValues) {
+                    id
+                }
+            }
+        `;
+
+        // Variables para la mutación
+        const variables = {
+            boardId: boardId,
+            itemName: name,
+            columnValues: columnValues
+        };
+
+        console.log('Creating new item with data:', columnValues);
+        const response = await axios.post('https://api.monday.com/v2', {
+            query: mutation,
+            variables: variables
+        }, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log('API response for new item creation:', response.data);
+        if (response.data && response.data.data && response.data.data.create_item && response.data.data.create_item.id) {
+            const newItemId = response.data.data.create_item.id;
+            console.log('New item created with ID:', newItemId);
+            return newItemId;
+        } else {
+            console.error('Failed to create new item. API response:', response.data);
+            throw new Error('Failed to create new item');
+        }
+    } catch (error) {
+        console.error('Error occurred while creating a new item:', error);
+        if (error.response) {
+            console.error('Error response:', error.response.data);
+            console.error('Status:', error.response.status);
+        }
         throw error;
     }
 }
 
 
+
 exports.contractToOt = async (req, res) => {
     try {
-        console.log("Inicio de la función");
+        console.log('Starting the function');
 
-        // Verificar que la solicitud tenga un cuerpo (body)
         if (!req.body) {
-            throw new Error('La solicitud no contiene datos en el cuerpo (body)');
+            throw new Error('The request does not contain data in the body');
         }
 
-        // Verificar que el cuerpo de la solicitud contenga un evento y un pulseId
         if (!req.body.event || !req.body.event.pulseId) {
-            throw new Error('La solicitud no contiene la estructura esperada de un evento de Monday.com');
+            throw new Error('The request does not contain the expected structure of a Monday.com event');
         }
 
-        const boardId = 5598495616; // Reemplaza esto con el ID correcto del tablero de destino
+        const boardId = 5598495616;
         const itemId = req.body.event.pulseId;
 
-        // Utiliza la nueva función para procesar subelementos y crear nuevos ítems
         await processSubElementsAndCreateItems(boardId, itemId);
 
-        res.status(200).send("Items procesados y nuevos items creados correctamente");
+        res.status(200).send('Subitems processed and new items created successfully');
     } catch (error) {
-        console.error("Error en la función principal:", error.message);
-        res.status(400).send("Error en la solicitud: " + error.message); // Cambiado a 400 Bad Request para errores de validación
+        console.error('Error in the main function:', error.message);
+        res.status(400).send('Error in the request: ' + error.message);
     }
 };
-
